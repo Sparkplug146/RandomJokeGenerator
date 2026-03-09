8e358da6806a8302ca59a19e662ef6238cd4feb9
Keoni Inniss <ika84465@gmail.com>
2026-03-08 21:14:15 -0400

    Updated html file to joke generator

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const button=document.getElementById("activityBtn");
+const button = document.getElementById("activityBtn");
+const activity = document.getElementById("activityName");
+const type = document.getElementById("activityType");
+const participants = document.getElementById("activityParticipants");
 
 button.addEventListener("click", function(){
     console.log("Generating Activity...")
+
+    fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 })
